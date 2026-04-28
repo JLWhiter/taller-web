1da6e3547a5cc365b9c0f25fcbbd80e95dd5f19a
devzerlogic <zer.principal@gmail.com>
2026-04-28 00:45:07 -0500

    Reestructuración de carpetas y nombre de archivos

--- login/main.js
+++ login/main.js
@@ -2,5 +2,5 @@ const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    window.location.href = "../inicio/first-page.html"
+    window.location.href = "../pagina-web/index.html"
 });
