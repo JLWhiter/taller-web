b07ab5b44fb49916596cf3773617a0df608f1c47
devzerlogic <zer.principal@gmail.com>
2026-05-05 01:18:28 -0500

    Integración de validacion del correo y contraseñas dentro de login.html

--- login/main.js
+++ login/main.js
@@ -1,6 +1,24 @@
+const emailInput = document.getElementById("email");
+const passwdInput = document.getElementById("password");
 const form = document.querySelector("form");
 
-form.addEventListener("submit", (e) => {
+// Validación de email y contraseña
+
+form.addEventListener("submit", function (e) {
     e.preventDefault();
-    window.location.href = "../web-site/index.html"
-});
+
+    const emailVal = emailInput.value.trim();
+    const passwdVal = passwdInput.value;
+
+    if (emailVal === "" || passwdInput === "") {
+        alert("Completa los campos!");
+        return;
+    }
+
+    if (emailVal !== "residente@correo.com" || passwdVal !== "resi123") {
+        alert("Correo o contraseña incorrecta!");
+        return;
+    }
+
+    window.location.href = "../web-site/index.html";
+})
